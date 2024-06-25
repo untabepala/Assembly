4ff81dd9dbcd7098efefa540cb272e0ad7452020
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-06-26 04:54:31 +0530

    Add files via upload

---
+++ mips4.asm
@@ -0,0 +1,18 @@
+.text
+.globl main
+
+main:
+li $v0, 1
+lw $t1, num1($zero)
+lw $t2, num2($zero)
+
+sub $t0,$t1,$t2  
+add $a0,$t0,$zero
+
+syscall
+li $v0, 10
+syscall
+
+.data
+num1: .word 2
+num2: .word 1
