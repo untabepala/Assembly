919c5e55199a5b5f3b5832eda47bd896cb7e7887
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2024-07-02 07:55:20 +0530

    Add files via upload

---
+++ mips10.asm
@@ -0,0 +1,27 @@
+.data
+questionMsg: .asciiz "Enter a NUMBER:"
+answerMsg: .asciiz "The number is:"
+
+.text
+.globl main
+
+main:
+#print the msg
+li $v0,4
+la $a0, questionMsg
+syscall
+
+#get the user input
+li $v0,5
+syscall
+
+move $t0, $v0
+
+#display the msg
+li $v0,4
+la $a0,answerMsg
+syscall
+
+li $v0, 1
+move $a0,$t0
+syscall
